7e250130439f087ada0ba7c3f5905a4e1968c91b
stephen azosike <stephenazosike@gmail.com>
2023-03-30 14:42:39 +0100

    yummly

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const fetch = require('node-fetch');
 
-
 const app = express();
-const port = 3000;
 app.use(express.json());
+const port = process.env.PORT || 3000;
 
 
-const url = 'https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup';
+app.get('/', async (req, res) => {
+    const url = 'https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'a341921e01msh06705c8228a50b8p1c874bjsnb51333fe1d85',
-    'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
-  }
-};
+    const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'a341921e01msh06705c8228a50b8p1c874bjsnb51333fe1d85',
+          'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+        }
+      };
 
-fetch(url, options)
-	.then(res => res.json())
-	.then(json => console.log(json))
-	.catch(err => console.error('error:' + err));
-
-app.get('/', (req, res) => {
-    res.send(url)
+    try {
+        let response = await fetch(url, options);
+        response = await response.json();
+        res.status(200).json(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({msg: `Internal Server Error.`});
+    }
 })
-
 
 // start
 app.listen(port, () => {
